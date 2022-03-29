d6add90dbd6ce914741ee6fe7bf19afaebe8baa8
Tanner Brieger <tanner29@gmail.com>
2022-03-28 22:31:58 -0500

    about me page setup, need to personalize still

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import heroImage from "../../assets/heroImage.jpg";
+//import heroImage from "../../assets/heroImage.jpg";
 
 function About() {
   return (
-    <section className='my-5'>
+    <section className='about'>
       <h1 id="about">Who am I?</h1>
-      <img src={heroImage} className="my-2" style={{ width: "100%" }} alt="cover" />
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing 
+        elit, sed do eiusmod tempor incididunt ut labore et 
+        dolore magna aliqua. Ut enim ad minim veniam, quis 
+        nostrud exercitation ullamco laboris nisi ut aliquip 
+        ex ea commodo consequat. Duis aute irure dolor in 
+        reprehenderit in voluptate velit esse cillum dolore 
+        eu fugiat nulla pariatur. Excepteur sint occaecat 
+        cupidatat non proident, sunt in culpa qui officia 
+        deserunt mollit anim id est laborum.  </p>
     </section>
   );
 }
