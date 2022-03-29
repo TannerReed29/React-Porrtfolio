64c6d49527150d4b486989362d4f5b57a6d7396c
Tanner Brieger <tanner29@gmail.com>
2022-03-28 22:42:45 -0500

    components imported and routes setup

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
-import React, { useState} from 'react';
+import React, {Component} from 'react';
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import './App.css';
+
+import Header from './components/Header';
 import About from './components/About';
-import Nav from './components/Nav';
-import ContactForm from './components/Contact';
-import Projects from './components/Projects';
+import Home from './components/Home';
+//import Nav from './components/Nav';
+import Contact from './components/Contact';
+import Project from './components/Project';
+import Footer from './components/Footer';
+import Resume from './components/Resume';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-
-function App() {
-  const [contactSelected, setContactSelected] = useState(false);
-  const [currentCategory, setCurrentCategory] = useState(categories[0]);
-  return (
-    <div>
-      <Nav
-        setCurrentCategory={setCurrentCategory}
-        contactSelected={contactSelected}
-        currentCategory={currentCategory}
-        setContactSelected={setContactSelected}
-        ></Nav>
-      <main>
-        {!contactSelected ? (
-          <>
-          <Projects></Projects>
-        <About></About>
-          </>
-        ) : (
-          <ContactForm></ContactForm>
-        )}
-      </main>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <HashRouter basename='/'>
+        <div className='app'>
+          <Header />
+          <main>
+            <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/about" element={<About />} />
+            <Route exact path="/contact" element={<Contact />} />
+            <Route exact path="/project" element={<Project />} />
+            <Route exact path="/resume" element={<Resume />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </HashRouter>
+    );
+  }
 }
 
 export default App;
