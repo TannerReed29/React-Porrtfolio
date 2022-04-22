015aa0121eee3b61e8186914e0d29f12f658e168
Tanner Brieger <tanner29@gmail.com>
2022-04-21 22:04:15 -0500

    setup checkedState and array for pwgen choices, need to add input check for length 8-128, then randomize final array of choices on button submit...

--- src/components/PasswordGen/index.js
+++ src/components/PasswordGen/index.js
@@ -1,90 +1,118 @@
 import React from "react";
 import { useState } from "react";
-
-
+import { pwgchoices } from '../../utils/pwgchoices';
+// import { validateEmail } from '../../utils/helpers';
 
 
 function PWGen() {
+const [checkedState, setCheckedState] = useState(
+  new Array(pwgchoices.length).fill(false)
+);
 
-var criteria = []; 
-
-const lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-const characters = ['"', '!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
+const [choice, setChoice] = useState("");
 
 
-   function generatePassword() {
-  // clear criteria 
-  criteria = [];
-  // Prompt user for password length | notify user if input is wrong
-  const plength = parseInt(window.prompt('Input a desired password length between 8 - 128'));
-  if (isNaN(plength))
-    {
-    alert("Input is not a number");
-    return null
-    }
-  else
-    {
-      if (plength >= 8 && plength <= 128)
-      {
-          console.log(plength);
-          }  else {
-            alert("Please input a number between 8 and 128");
-            return null
+const handleOnChange = (position) => {
+  const updateCheckedState = checkedState.map((item, index) =>
+    index === position ? !item : item
+  );
+
+  setCheckedState(updateCheckedState);
+
+  const choices = updateCheckedState.reduce(
+    (passArr, currentState, index) => {
+      if (currentState === true) {
+        console.log(pwgchoices[index].choice);
+        return passArr + pwgchoices[index].choice;
       }
-    }
+      return passArr;
+    },
+    );
+  // console.log(pwgchoices);
+  setChoice(choices);
+    console.log(choice);
 
-    // Confirm if user wants lowercase 
-    const lowercase = window.confirm('Would you like to include Lowercase?');
-    if (lowercase) {
-      criteria = criteria.concat(lower);
-    }
+};
 
-    // Confirm if user wants lowercase
-    const uppercase = window.confirm('Would you like to include Uppercase?');
-    if (uppercase) {
-      criteria = criteria.concat(upper);
-    }
+// var criteria = []; 
 
-    // Confirm if user wants numbers
-    const numeric = window.confirm('Would you like to include Numbers?');
-    if (numeric) {
-      criteria = criteria.concat(numbers);
-    }
+// const lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+// const upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+// const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// const characters = ['"', '!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
 
-    // Confirm if user wants special characters
-    const special = window.confirm('Would you like to include Special Charcters?');
-    if (special) {
-      criteria = criteria.concat(characters);
-    }
 
-    // password is generated here based on chosen criteria
-    var genpass = "";
-    for(var i = 0; i < plength; i++) {
-      var random = Math.floor(Math.random() * criteria.length);
-      genpass = genpass + criteria[random];
-    }
-    // return constiable of generated password here
-    return genpass;
-  }
+  //  function generatePassword() {
+  // // clear criteria 
+  // criteria = [];
+  // // Prompt user for password length | notify user if input is wrong
+  // const plength = parseInt(window.prompt('Input a desired password length between 8 - 128'));
+  // if (isNaN(plength))
+  //   {
+  //   alert("Input is not a number");
+  //   return null
+  //   }
+  // else
+  //   {
+  //     if (plength >= 8 && plength <= 128)
+  //     {
+  //         console.log(plength);
+  //         }  else {
+  //           alert("Please input a number between 8 and 128");
+  //           return null
+  //     }
+  //   }
+
+  //   // Confirm if user wants lowercase 
+  //   const lowercase = window.confirm('Would you like to include Lowercase?');
+  //   if (lowercase) {
+  //     criteria = criteria.concat(lower);
+  //   }
+
+  //   // Confirm if user wants lowercase
+  //   const uppercase = window.confirm('Would you like to include Uppercase?');
+  //   if (uppercase) {
+  //     criteria = criteria.concat(upper);
+  //   }
+
+  //   // Confirm if user wants numbers
+  //   const numeric = window.confirm('Would you like to include Numbers?');
+  //   if (numeric) {
+  //     criteria = criteria.concat(numbers);
+  //   }
+
+  //   // Confirm if user wants special characters
+  //   const special = window.confirm('Would you like to include Special Charcters?');
+  //   if (special) {
+  //     criteria = criteria.concat(characters);
+  //   }
+
+  //   // password is generated here based on chosen criteria
+  //   var genpass = "";
+  //   for(var i = 0; i < plength; i++) {
+  //     var random = Math.floor(Math.random() * criteria.length);
+  //     genpass = genpass + criteria[random];
+  //   }
+  //   // return constiable of generated password here
+  //   return genpass;
+  // }
   
 
   
 
 
 // Get references to the #generate element
-const generateBtn = document.querySelector("#generate");
+// const generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function WritePassword () {
-    const password = generatePassword();
-    var passwordText = document.querySelector("#password");
+// function WritePassword () {
+//     const password = genPass();
+//     var passwordText = document.querySelector("#password");
     
-    passwordText.value = password;
+//     passwordText.value = password;
     
-    generateBtn.window.addEventListener("submit", WritePassword);
-}
+    // generateBtn.window.addEventListener("submit", WritePassword);
+// }
 
 
 // Add event listener to generate button
@@ -121,15 +149,43 @@ function WritePassword () {
                         <h2>Generate a Password</h2>
                             </div>
                     <div class="card-body">
+                        <h3>Select Password Criteria</h3>
+                        <ul className="pwgen-list">
+                          {pwgchoices.map(({ name }, index) => {
+                            return (
+                              <li key={index}>
+                                <div className="pwgen-list-item">
+                                  <div className="left-section">
+                                    <input
+                                      type="checkbox"
+                                      id={`custom-checkbox-${index}`}
+                                      name={name}
+                                      value={name}
+                                      checked={checkedState[index]}
+                                      onChange={() => handleOnChange(index)} 
+                                    />
+                                    <label htmlFor={`custom-checkbox-${index}`}>{name}</label>
+                                  </div>
+                                </div>
+                              </li>
+                            )
+                          })}
+                        </ul>
                         <textarea
                             readOnly
                             id="password"
                             placeholder="Your Secure Password"
                             aria-label="Generated Password"
-                        ></textarea>
+                        >
+                         
+                          {/* {WritePassword()} */}
+                          
+                          
+                          </textarea>
                     </div>
                     <div class="card-footer">
-                        <button type="submit" id="generate" class="btn btn-success" onSubmit={WritePassword}>Generate Password</button>
+                        <button type="submit" id="generate" class="btn btn-success" >Generate Password</button>
+                        {/* <button type="submit" id="generate" class="btn btn-success" onSubmit={handleSubmit}>Generate Password</button> */}
                     </div>
                 </div>
             </div>
---
+++ src/utils/pwgchoices.js
@@ -0,0 +1,22 @@
+export const pwgchoices = [
+    {
+        name: "Would you like to include Lowercase?",
+        choice: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    },
+    {
+        name: "Would you like to include Uppercase?",
+        choice: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+    },
+    {
+        name: "Would you like to include Numbers?",
+        choice:['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+    },
+    {
+        name: "Would you like to include Special Charcters?",
+        choice: ['"', '!', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~']
+    },
+];
+
+
